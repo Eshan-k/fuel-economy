2911c46755359ac3b5855b1878c22d1828fdb58e
Eshan K <eshank@Eshans-MacBook-Pro.local>
2019-06-14 14:40:14 -0400

    changes

--- fuel-economy.py
+++ fuel-economy.py
@@ -10,8 +10,25 @@ df_18 = pd.read_excel('y18.xlsx')
 # print(df_18.head())
 
 # drop irrelevant columns
-#df_08.drop(['Stnd', 'Underhood ID', 'FE Calc Appr', 'Unadj Cmb MPG'], axis=1, inplace=True)
-#print(df_08.head())
+df_08.drop(['Stnd', 'Underhood ID', 'FE Calc Appr', 'Unadj Cmb MPG'], axis=1, inplace=True)
+print(df_08.head())
 df_18.drop(['Stnd', 'Stnd Description', 'Underhood ID', 'Comb CO2'], axis=1, inplace=True)
 print(df_18.head())
 
+# rename Sales Area to Cert Region
+df_08.rename(columns={'Sales Area': 'Cert Region'}, inplace=True)
+df_08.head(2)
+
+# replace spaces with underscores and lowercase labels for 2008 & 2018 dataset
+df_08.rename(columns=lambda x: x.strip().lower().replace(" ", "_"), inplace=True)
+df_08.head(2)
+
+df_18.rename(columns=lambda x: x.strip().lower().replace(" ", "_"), inplace=True)
+df_18.head(2)
+
+# check labels for 2008 and 2018 datasets are identical
+(df_08.columns == df_18.columns).all()
+
+# save the datasets
+df_08.to_csv('data_08.csv', index=False)
+df_18.to_csv('data_18.csv', index=False)
