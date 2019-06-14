aabba196643e6e5ba99d794744c7411805f465a8
Eshan K <eshank@Eshans-MacBook-Pro.local>
2019-06-14 13:40:55 -0400

    fix: dropped columns

--- fuel-economy.py
+++ fuel-economy.py
@@ -10,5 +10,8 @@ df_18 = pd.read_excel('y18.xlsx')
 # print(df_18.head())
 
 # drop irrelevant columns
-df_08.drop(['Stnd'], ['Underhood ID'], ['FE Calc Appr'], ['Unadj Cmb MPG'])
-df_18.drop(['Stnd'], ['Stnd Description'], ['Underhood ID'], ['Comb CO2'])
+#df_08.drop(['Stnd', 'Underhood ID', 'FE Calc Appr', 'Unadj Cmb MPG'], axis=1, inplace=True)
+#print(df_08.head())
+df_18.drop(['Stnd', 'Stnd Description', 'Underhood ID', 'Comb CO2'], axis=1, inplace=True)
+print(df_18.head())
+
